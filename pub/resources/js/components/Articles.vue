008b503ffbb56e04050d6040c343558b477e64a8
Ronald <tdng91@gmail.com>
2019-09-09 14:35:02 +0700

    complete app with CRUD

--- pub/resources/js/components/Articles.vue
+++ pub/resources/js/components/Articles.vue
@@ -1,16 +1,38 @@
 <template>
     <div>
         <h2>Articles</h2>
+
+        <form @submit.prevent="addArticle" class="mb-3">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Title" v-model="article.title"/>
+            </div>
+            <div class="form-group">
+                <textarea type="text" class="form-control" placeholder="Body" v-model="article.body"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-light btn-block">Save</button>
+        </form>
+
         <nav aria-label="Page navigation example">
             <ul class="pagination">
-                <li class="page-item" v-bind:class="[{disabled: !pagination.prev_page_url}]"><a class="page-link" href="#" @click="fetchArticles(pagination.prev_page_url)">Previous</a></li>
-                <li class="page-item disabled"><a class="page-link text-dark" href="#">Page {{pagination.current_page}} of {{pagination.last_page}}</a></li>
-                <li class="page-item" v-bind:class="[{disabled: !pagination.next_page_url}]"><a class="page-link" href="#" @click="fetchArticles(pagination.next_page_url)">Next</a></li>
+                <li class="page-item" v-bind:class="[{disabled: !pagination.prev_page_url}]"><a class="page-link"
+                                                                                                href="#"
+                                                                                                @click="fetchArticles(pagination.prev_page_url)">Previous</a>
+                </li>
+                <li class="page-item disabled"><a class="page-link text-dark" href="#">Page {{pagination.current_page}}
+                    of {{pagination.last_page}}</a></li>
+                <li class="page-item" v-bind:class="[{disabled: !pagination.next_page_url}]"><a class="page-link"
+                                                                                                href="#"
+                                                                                                @click="fetchArticles(pagination.next_page_url)">Next</a>
+                </li>
             </ul>
         </nav>
         <div class="card card-body mb-2" v-for="article in articles" v-bind:key="article.id">
             <h3>{{article.title}}</h3>
             <p>{{article.body}}</p>
+            <hr>
+            <button @click="editArticle(article)" class="btn btn-warning mb-2">Edit</button>
+            <button @click="deleteArticle(article.id)" class="btn btn-danger">Delete</button>
         </div>
     </div>
 </template>
@@ -54,6 +76,66 @@
                 };
 
                 this.pagination = pagination;
+            },
+            deleteArticle(id) {
+                if (confirm('Are You Sure?')) {
+                    fetch(`api/article/${id}`, {
+                        method: 'delete'
+                    })
+                        .then(res => res.json)
+                        .then(data => {
+                            alert('Article deleted');
+                            this.fetchArticles();
+                        })
+                        .catch(err => console.log(err));
+                }
+            },
+            addArticle() {
+                if (this.edit === false) {
+                    // Add
+                    fetch('api/article', {
+                        method: 'post',
+                        body: JSON.stringify(this.article),
+                        headers: {
+                            'content-type': 'application/json'
+                        }
+                    })
+                        .then(res => res.json)
+                        .then(data => {
+                            this.article.title = '';
+                            this.article.body = '';
+                            alert('Article Added');
+                            this.fetchArticles();
+                        })
+                        .catch(err => console.log(err));
+                } else {
+                    // Update
+                    fetch('api/article', {
+                        method: 'put',
+                        body: JSON.stringify(this.article),
+                        headers: {
+                            'content-type': 'application/json'
+                        }
+                    })
+                        .then(res => res.json)
+                        .then(data => {
+                            this.article.title = '';
+                            this.article.body = '';
+                            alert('Article Updated');
+                            this.fetchArticles();
+                        })
+                        .catch(err => console.log(err));
+
+                }
+            },
+            editArticle(article) {
+                this.edit = true;
+                this.article.id = article.id;
+                this.article.article_id = article.id;
+                this.article.title = article.title;
+                this.article.body = article.body;
+
+
             }
         }
     }
